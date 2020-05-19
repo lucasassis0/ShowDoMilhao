fd806e366ba615982174882451265cea56dac873
Lucas Assis <lucas.assis_@hotmail.com>
2020-05-18 20:18:39 -0400

    Primeiro commit

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class App extends React.Component{
+
+
+  render(){
+    return(
+      <View>
+        <Text></Text>
+      </View>
+    )
+  }
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
