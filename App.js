1664089caec2b1429f448addc52bfc46c4764077
Evandro Carlos <evandrorocha20@gmail.com>
2020-05-26 20:57:07 -0300

    tela de login

--- App.js
+++ App.js
@@ -1,34 +1,88 @@
-import 'react-native-gesture-handler'
-import React from 'react';
+import React from 'react'
+import { Text, View, Button, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native'
 
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
+class Login extends React.Component {
 
-import Inicio from './src/views/Inicio'
-import Login from './src/views/Login'
-
-const Stack = createStackNavigator()
-
-class App extends React.Component{
-
-  render(){
-    return(
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Inicio" component={Inicio}
-            options={{title: ' ', headerStyle: {
-              backgroundColor: 'blue',
-              borderBottomColor: 'blue',
-              borderBottomWidth: 1
-            }}}
-          />
-          <Stack.Screen name='Login' component={Login}
-          
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flex: 1 }} >
+          <Image style={styles.logo} source={require('./src/imgs/milhao.png')} />
+          <View style={styles.caixaDeLogin}>
+            <View><Text style={styles.textoEmail}>E-Mail </Text></View>
+            <TextInput style={styles.entradaDeTexto}
+              placeholder='  Seu e-mail'
+            />
+            <View><Text style={styles.textoSenha}>Senha </Text></View>
+            <TextInput style={styles.entradaDeTexto}
+              placeholder='  Senha'
+            />
+          </View>
+          <TouchableOpacity>
+            <Text style={styles.botaoCadastrar}>Cadastrar-se</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.botaoCadastrar}>Esqueceu a Senha?</Text>
+          </TouchableOpacity>
+          <View>
+          <TouchableOpacity>
+            <Text Entrar></Text>
+          </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     )
   }
 }
 
-export default App
+export default Login
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#172178',
+    flex: 1,
+  },
+  logo: {
+    flex: 0.2,
+    width: 180,
+    alignSelf: 'center',
+    margin: 30
+  },
+  caixaDeLogin: {
+    width: 320,
+    flex: 0.4,
+    backgroundColor: '#EEF431',
+    alignSelf: 'center',
+    borderRadius: 15,
+    borderColor: 'gold',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+  },
+  entradaDeTexto: {
+    width: '98%',
+    height: 30,
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 5,
+    marginVertical: 8,
+    marginHorizontal: 2,
+    backgroundColor: '#c4c4c4'
+  },
+  textoEmail: {
+    paddingHorizontal: 6,
+    marginTop: 20,
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  textoSenha: {
+    paddingHorizontal: 6,
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  botaoCadastrar: {
+    textAlign: 'center',
+    color: '#C8CE1A',
+    marginTop: 7,
+    fontSize: 20
+  }
+})
