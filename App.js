dc546ad5825f3173cd6847494d996693233c0d1d
Evandro Carlos <evandrorocha20@gmail.com>
2020-05-27 16:22:43 -0300

    tela de parar concluída e as funcionalidades dos botoes

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import 'react-native-gesture-handler'
 import * as React from 'react'
-import {Text, View, Button} from 'react-native'
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
+import { Text, View, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import PaginaLogin from './src/pages/paginaLogin'
 import PaginaHome from './src/pages/paginaHome'
@@ -13,19 +13,22 @@ import PaginaRanking from './src/pages/paginaRanking'
 import PaginaFimDeJogo from './src/pages/paginaFimDeJogo'
 
 
-const Stack = createStackNavigator()  
+const Stack = createStackNavigator()
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="PaginaHome"> 
+      <Stack.Navigator initialRouteName="Parou!">
         <Stack.Screen name="Login" component={PaginaLogin} />
         <Stack.Screen name="PaginaHome" component={PaginaHome} />
         <Stack.Screen name="PaginaJogo" component={PaginaJogo} />
         <Stack.Screen name="PaginaPerfil" component={PaginaPerfil} />
         <Stack.Screen name="PaginaCadastraJodador" component={PaginaCadastraJogador} />
         <Stack.Screen name="PaginaRanking" component={PaginaRanking} />
-        <Stack.Screen name="PaginaFimDeJogo" component={PaginaFimDeJogo} />
+        <Stack.Screen name="Parou!" component={PaginaFimDeJogo}
+          options={{
+            title: ' ', headerTransparent: 1
+          }} />
       </Stack.Navigator>
     </NavigationContainer>
   )
